5127d9fea9c9441300f5bafd7b2f27e2467767de
Rob Gonzalez-Pita <rob.gonzalez-pita@noaa.gov>
2021-04-23 12:23:03 -0600

    Update comparison tests from merging develop branch

--- Julia/test/compare_tests.jl
+++ Julia/test/compare_tests.jl
@@ -5,19 +5,21 @@
 
 using Test
 
-inputfiles = readdir("../test/baselines", join=true)
+inputfiles = readdir("../test/test_output", join=true)
 
 outputfiles = readdir("test/test_output", join=true)
 
 # Filter the files to only include non_interpolated_files
-non_interpolated_input_files = filter(p->p[30] == '0', inputfiles)
+non_interpolated_input_files = filter(p->p[32] == '0', inputfiles)
 non_interpolated_output_files = filter(p->p[29] == '0', outputfiles)
+
+println(inputfiles)
 
 for (input, output) in zip(non_interpolated_input_files, non_interpolated_output_files)
 
 # Once interpolation is working, remove three lines above and use below 'for' loop
 # for (input, output) in zip(inputfiles, outputfiles)
-    datasize = input[19:30]
+    datasize = input[21:32]
     @testset "$datasize Comparison " begin
 
         @test success(`cmp --quiet $input $output`)
