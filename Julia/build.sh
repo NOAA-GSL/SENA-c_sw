adef2b0226fe7c0274c9a438a4c97ce617fc9511
Rob Gonzalez-Pita <rob.gonzalez-pita@noaa.gov>
2021-04-27 17:43:43 -0600

    Update direcrtories for NetCDF output files in CI and build.sh

--- Julia/build.sh
+++ Julia/build.sh
@@ -6,6 +6,6 @@ julia --project=. -e 'using Pkg; Pkg.instantiate()'
 
 julia --project=. -e 'mkdir("test/test_output")'
 
-julia --project=. -e 'mkpath("../data/outputs")'
+julia --project=. -e 'mkdir("../data/outputs")'
 
 julia --project=. test/test_c_sw.jl
